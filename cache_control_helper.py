3a6f1099a3bb326dab5e23fdd70f45ebeb72f4be
DeqingQu <qu.deqing@gmail.com>
2019-01-18 15:45:13 -0800

    refactor the cache_control_helper

--- cache_control_helper.py
+++ cache_control_helper.py
@@ -10,15 +10,13 @@ import time
 
 class CustomHeuristic(BaseHeuristic):
 
-    def __init__(self, days: float = ..., seconds: float = ..., minutes: float = ..., hours: float = ...):
+    def __init__(self, days: float = ...):
         self.days = days
-        self.seconds = seconds
-        self.minutes = minutes
-        self.hours = hours
 
     def update_headers(self, response):
         date = parsedate(response.headers['date'])
-        expires = datetime(*date[:6]) + timedelta(seconds=self.seconds)
+        expires = datetime(*date[:6]) + timedelta(seconds=self.days)
+        print(formatdate(calendar.timegm(expires.timetuple())))
         return {
             'expires': formatdate(calendar.timegm(expires.timetuple())),
             'cache-control': 'public',
@@ -29,23 +27,36 @@ class CustomHeuristic(BaseHeuristic):
         return '110 - "%s"' % msg
 
 
-if __name__ == '__main__':
+class CacheControlHelper(object):
+
+    def __init__(self):
+        self.sess = CacheControl(Session(), heuristic=CustomHeuristic(days=30), cache=FileCache('.web_cache'))
+
+    def get(self, url):
+        return self.sess.get(url)
+
+
+def test_case():
     url0 = 'http://cohd.io/api/association/obsExpRatio?dataset_id=1&concept_id_1=192855&domain=Procedure'
     url1 = 'http://cohd.io/api/association/obsExpRatio?dataset_id=1&concept_id_1=192853&domain=Procedure'
     url2 = 'http://cohd.io/api/association/obsExpRatio?dataset_id=1&concept_id_1=192854&domain=Procedure'
 
-    sess = CacheControl(Session(), heuristic=CustomHeuristic(seconds=30), cache=FileCache('.web_cache'))
+    req = CacheControlHelper()
 
     t = time.time()
-    r = sess.get(url1)
+    r = req.get(url0)
     print(time.time() - t)
     print(r.from_cache)
     t = time.time()
-    r = sess.get(url1)
+    r = req.get(url1)
     print(time.time() - t)
     print(r.from_cache)
     t = time.time()
-    r = sess.get(url1)
+    r = req.get(url2)
     print(time.time() - t)
     print(r.from_cache)
+
+
+if __name__ == '__main__':
+    test_case()
 
