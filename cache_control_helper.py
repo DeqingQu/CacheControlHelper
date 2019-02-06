d9425ff08253f10ef1028b724418c705f8d58bf5
DeqingQu <qu.deqing@gmail.com>
2019-02-06 12:33:35 -0800

    add parameters for get and post method

--- cache_control_helper.py
+++ cache_control_helper.py
@@ -15,7 +15,7 @@ class CustomHeuristic(BaseHeuristic):
     def update_headers(self, response):
         date = parsedate(response.headers['date'])
         expires = datetime(*date[:6]) + timedelta(days=self.days)
-        print(formatdate(calendar.timegm(expires.timetuple())))
+        # print(formatdate(calendar.timegm(expires.timetuple())))
         return {
             'expires': formatdate(calendar.timegm(expires.timetuple())),
             'cache-control': 'public',
@@ -32,11 +32,11 @@ class CacheControlHelper(object):
         self.sess = CacheControl(requests.session(), heuristic=CustomHeuristic(days=30), cache=FileCache('.web_cache'))
         self.exceptions = requests.exceptions
 
-    def get(self, url, timeout=120, cookies=None):
+    def get(self, url, params=None, timeout=120, cookies=None, headers={'Accept': 'application/json'}):
         if cookies:
-            return self.sess.get(url, timeout=timeout, cookies=cookies)
+            return self.sess.get(url, params=params, timeout=timeout, cookies=cookies, headers=headers)
         else:
-            return self.sess.get(url, timeout=timeout)
+            return self.sess.get(url, params=params, timeout=timeout, headers=headers)
 
-    def post(self, url, data):
-        return self.sess.post(url, data=data)
+    def post(self, url, data, timeout=120, headers={'Accept': 'application/json'}):
+        return self.sess.post(url, data=data, timeout=timeout, headers=headers)
